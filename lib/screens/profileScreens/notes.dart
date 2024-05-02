754f742cb1ea9a9b2b82f16e810b46b8c77a32a7
Amira Gabr <91220375+amiraegabr@users.noreply.github.com>
2024-05-02 21:48:39 +0300

    improve

--- lib/screens/profileScreens/notes.dart
+++ lib/screens/profileScreens/notes.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Notes extends StatelessWidget {
+class Notes extends StatefulWidget {
   const Notes({super.key});
+
+  @override
+  _NotesState createState() => _NotesState();
+}
+
+class _NotesState extends State<Notes> {
+  final List<String> _notes = [];
+  final TextEditingController _noteController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,10 +17,32 @@ class Notes extends StatelessWidget {
       appBar: AppBar(),
       body: Column(
         children: [
-          Text("NO NOTES YET"),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _notes.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(_notes[index]));
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _noteController,
+              decoration: const InputDecoration(hintText: 'Enter a note'),
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                _notes.add(_noteController.text);
+                _noteController.clear();
+              });
+            },
+            child: const Icon(Icons.add),
+          ),
         ],
       ),
-
     );
   }
 }
