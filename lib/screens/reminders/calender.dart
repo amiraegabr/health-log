66c84a6145a63bd1b6470018893b5ac6bd90566c
Amira Gabr <91220375+amiraegabr@users.noreply.github.com>
2024-05-02 21:48:37 +0300

    improve

--- lib/screens/reminders/calender.dart
+++ lib/screens/reminders/calender.dart
@@ -1,63 +1,43 @@
 import 'package:flutter/material.dart';
-
-// import 'package:healthlog/reminders/remindersAppBar.dart';
-//
-// class RemindersPage extends StatefulWidget {
-//   const RemindersPage({super.key});
-//
-//   @override
-//   State<RemindersPage> createState() => _RemindersPageState();
-// }
-//
-// class _RemindersPageState extends State<RemindersPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: RemindersAppBar(),
-//       body: TabBarView(
-//         children: [
-//
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatefulWidget {
-  const Calendar({super.key});
+class RemindersPage extends StatefulWidget {
+  const RemindersPage({super.key});
 
   @override
-  State<Calendar> createState() => _CalendarState();
+  State<RemindersPage> createState() => _RemindersPageState();
 }
 
-class _CalendarState extends State<Calendar> {
-  DateTime today = DateTime.now();
+class _RemindersPageState extends State<RemindersPage> {
+  DateTime focusedDay = DateTime.now();
 
-  void _onDaySelected(DateTime day, DateTime focusedDay) {
+  void _onDaySelected(DateTime day) {
     setState(() {
-      today = day;
+      focusedDay = day;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      focusedDay: today,
-      firstDay: DateTime.utc(2020),
-      lastDay: DateTime.utc(2030),
-      onDaySelected: _onDaySelected,
-      calendarFormat: CalendarFormat.twoWeeks,
-      headerStyle: const HeaderStyle(
-        formatButtonVisible: false,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Reminders')),
+      body: Column(
+        children: [
+          TableCalendar(
+            focusedDay: focusedDay,
+            firstDay: DateTime.utc(2020),
+            lastDay: DateTime.utc(2030),
+            onDaySelected: _onDaySelected,
+            calendarFormat: CalendarFormat.twoWeeks,
+            headerStyle: const HeaderStyle(
+              formatButtonVisible: false,
+            ),
+            availableGestures: AvailableGestures.all,
+            selectedDayPredicate: (day) => isSameDay(day, focusedDay),
+          ),
+          // Add other widgets for reminders here
+        ],
       ),
-      availableGestures: AvailableGestures.all,
-      // locale: "en_US",
-      // rowHeight: 35,
-
-      selectedDayPredicate: (day) => isSameDay(day, today),
-
     );
   }
 }
