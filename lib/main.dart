8272b344c19b324d192a534a69fe04dd1dde50ee
Amira Gabr <91220375+amiraegabr@users.noreply.github.com>
2024-05-02 21:48:41 +0300

    improve

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,62 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:healthlog/firebase_options.dart';
+import 'package:healthlog/screens/home.dart';
 import 'package:healthlog/screens/onboardingScreens/onboarding.dart';
 import 'package:healthlog/screens/splash.dart';
 import 'themes.dart';
 
-void main() async{
-  // firebase initialization
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const App());
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(const App());
+  } catch (e) {
+    print('Firebase initialization failed: $e');
+  }
 }
 
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       title: "Health log",
-      home: const SplashScreen(),
+      home: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const HomeScreen();
+          } else {
+            return const SplashScreen();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Health log'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Health log!'),
+      ),
     );
   }
 }
