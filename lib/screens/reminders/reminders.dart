6f24234f0e784af30cae45aad87a7f464f59c667
Amira Gabr <91220375+amiraegabr@users.noreply.github.com>
2024-05-02 21:48:43 +0300

    improve

--- lib/screens/reminders/reminders.dart
+++ lib/screens/reminders/reminders.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:healthlog/screens/reminders/appointments_page.dart';
 import 'package:healthlog/screens/reminders/medications_page.dart';
 import 'package:healthlog/screens/settings.dart';
+import 'package:healthlog/utils/constants.dart';
 import 'add_medication.dart';
-
 
 class Reminders extends StatefulWidget {
   const Reminders({super.key});
@@ -13,12 +13,11 @@ class Reminders extends StatefulWidget {
 }
 
 class _RemindersState extends State<Reminders> {
-  bool? status = false;
-  DateTime today = DateTime.now();
+  DateTime selectedDate = DateTime.now();
 
   void _onDaySelected(DateTime day, DateTime focusedDay) {
     setState(() {
-      today = day;
+      selectedDate = day;
     });
   }
 
@@ -29,8 +28,6 @@ class _RemindersState extends State<Reminders> {
       child: Scaffold(
         appBar: AppBar(
           title: const Text("Reminders"),
-          // leading: IconButton(
-          //     onPressed: () {}, icon: (const Icon(Icons.arrow_back_ios))),
           actions: <Widget>[
             IconButton(
               icon: const Icon(Icons.settings),
@@ -40,57 +37,57 @@ class _RemindersState extends State<Reminders> {
             ),
           ],
           bottom: TabBar(
-              // physics: const ClampingScrollPhysics(),
-              padding: const EdgeInsets.only(
-                left: 20,
-                right: 20,
-              ),
-              labelPadding: EdgeInsets.zero,
-              unselectedLabelColor: Theme.of(context).primaryColor,
-              indicator: BoxDecoration(
-                // borderRadius: BorderRadius.horizontal(left: Radius.circular(20)),
-                borderRadius: BorderRadius.circular(20),
-                color: Theme.of(context).splashColor,
-              ),
-              tabs: [
-                Tab(
-                  child: Container(
-                    height: 60,
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        bottomLeft: Radius.circular(20),
-                      ),
-                      border: Border.all(
-                          color: const Color.fromRGBO(255, 168, 115, 1),
-                          width: 1),
+            padding: const EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            labelPadding: EdgeInsets.zero,
+            unselectedLabelColor: kPrimaryColor,
+            indicator: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: kSplashColor,
+            ),
+            tabs: [
+              Tab(
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
                     ),
-                    // color: Theme.of(context).primaryColor,
-                    // ),
-                    child: const Align(
-                        alignment: Alignment.center,
-                        child: Text("Medications")),
+                    border: Border.all(
+                      color: const Color.fromRGBO(255, 168, 115, 1),
+                      width: 1,
+                    ),
+                  ),
+                  child: const Align(
+                    alignment: Alignment.center,
+                    child: Text("Medications"),
                   ),
                 ),
-                Tab(
-                  child: Container(
-                    height: 60,
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.only(
-                        topRight: Radius.circular(20),
-                        bottomRight: Radius.circular(20),
-                      ),
-                      border: Border.all(
-                          color: const Color.fromRGBO(255, 168, 115, 1),
-                          width: 1),
-                      // color: Theme.of(context).primaryColor,
+              ),
+              Tab(
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
                     ),
-                    child: const Align(
-                        alignment: Alignment.center,
-                        child: Text("Appointments")),
+                    border: Border.all(
+                      color: const Color.fromRGBO(255, 168, 115, 1),
+                      width: 1,
+                    ),
+                  ),
+                  child: const Align(
+                    alignment: Alignment.center,
+                    child: Text("Appointments"),
                   ),
                 ),
-              ]),
+              ),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
@@ -101,12 +98,12 @@ class _RemindersState extends State<Reminders> {
               ),
             );
           },
-          backgroundColor: const Color(0xFF129A7F),
+          backgroundColor: kPrimaryColor,
           child: const Icon(Icons.add),
         ),
-        body: const TabBarView(
+        body: TabBarView(
           children: [
-            MedicationsPage(),
+            MedicationsPage(selectedDate: selectedDate),
             AppointmentsPage(),
           ],
         ),
